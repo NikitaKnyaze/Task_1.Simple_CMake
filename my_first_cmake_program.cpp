918feda490069bbc70f119bb2d8a78fae4d1abdc
Knyazev Nikita <nk8222138@gmail.com>
2024-02-06 22:55:44 +0300

    Добавьте файлы проекта.

---
+++ my_first_cmake_program.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <windows.h>
+
+int main(){
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::cout << "Введите имя: ";
+	std::string name;
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name <<"!\n";
+
+	return 0;
+}
